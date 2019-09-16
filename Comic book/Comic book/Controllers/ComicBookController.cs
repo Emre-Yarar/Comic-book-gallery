58187a60af25a63d3ccbde72cdb2ccef2e8e0723
Emre95 <emre.yarar2508@gmail.com>
2019-09-16 21:34:38 +0200

    Added controller

---
+++ Comic book/Comic book/Controllers/ComicBookController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web;
+
+namespace Comic_book.Controllers
+{
+    public class ComicBookController : Controller
+    {
+
+    }
+}
