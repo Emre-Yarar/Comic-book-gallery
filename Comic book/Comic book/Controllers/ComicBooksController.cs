a21348b93b7702b5138dde557ee4551c99f236f5
Emre95 <emre.yarar2508@gmail.com>
2019-09-18 16:01:06 +0200

    Updated comic books controller to use repository

--- Comic book/Comic book/Controllers/ComicBooksController.cs
+++ Comic book/Comic book/Controllers/ComicBooksController.cs
@@ -4,27 +4,28 @@ using System.Linq;
 using System.Web.Mvc;
 using System.Web;
 using Comic_book.Models;
+using Comic_book.Data;
 
 namespace Comic_book.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
-        {
-            var comicBook = new ComicBook()
+
+        private ComicBookRepository _comicBookRepository = null;
+
+            public ComicBooksController()
             {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist()  { Name = "Dan Slott", Role = "Script"},
-                    new Artist()  { Name = "Humberto Ramos", Role = "Pencils"},
-                    new Artist()  { Name = "Victor Olazaba", Role = "Inks"},
-                    new Artist()  { Name = "Colors", Role = "Edgar Delgado"},
-                    new Artist()  { Name = "Letters", Role = "Chris Eliopoulos"},
-                }
-            };
+                _comicBookRepository = new ComicBookRepository();
+            }
+
+
+        public ActionResult Detail(int? id)
+        {
+            if(id == null)
+            {
+                return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook(id.Value);
 
 
             return View(comicBook);
